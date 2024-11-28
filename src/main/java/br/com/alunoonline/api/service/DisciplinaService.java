b784665b78b216970dbae32ecd1c21c8c32c6b7c
eukellyanne <kellyannekaren23@gmail.com>
2024-11-27 21:31:45 -0300

    Concluído: Desenvolvimento da API Aluno Online

--- src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
+++ src/main/java/br/com/alunoonline/api/service/DisciplinaService.java
@@ -3,9 +3,12 @@ package br.com.alunoonline.api.service;
 import br.com.alunoonline.api.model.Disciplina;
 import br.com.alunoonline.api.repository.DisciplinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DisciplinaService {
@@ -17,9 +20,35 @@ public class DisciplinaService {
         disciplinaRepository.save(disciplina);
     }
 
-    public List<Disciplina> listarDisciplinasdoProf (Long professorId) {
-
-        return List.of();
+    public List<Disciplina> listarTodasDisciplinas() {
+        return disciplinaRepository.findAll();
     }
+
+    public Optional<Disciplina> buscarDisciplinaPorId(Long id) {
+        return disciplinaRepository.findById(id);
+    }
+
+    public void deletarDisciplinaPorId(Long id) {
+        disciplinaRepository.deleteById(id);
+    }
+
+    public void atualizarDisciplinaPorId(Long id, Disciplina disciplina) {
+        Optional<Disciplina> disciplinaDoBancoDeDados = buscarDisciplinaPorId(id);
+
+        if (disciplinaDoBancoDeDados.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Disciplina não encontrada no banco de dados");
+        }
+
+        Disciplina disciplinaEditada = disciplinaDoBancoDeDados.get();
+        disciplinaEditada.setNome(disciplina.getNome());
+        disciplinaEditada.setProfessor(disciplina.getProfessor());
+
+        disciplinaRepository.save(disciplinaEditada);
+    }
+
+    public List<Disciplina> listarDisciplinasDoProf(Long professorId) {
+        return disciplinaRepository.findByProfessorId(professorId);
+    }
+
 }
 
